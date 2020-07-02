92123e5076023301b21e9db62d2d7dd2ffe42b25
Kendra <haanken@mail.gvsu.edu>
2020-07-02 16:55:51 -0400

    Added background to settings page

--- app/src/main/java/com/example/myfirstapp/SettingsPage.java
+++ app/src/main/java/com/example/myfirstapp/SettingsPage.java
@@ -14,7 +14,21 @@ public class SettingsPage extends PreferenceFragmentCompat {
 //            Bundle savedInstanceState
 //    ) {
 //        // Inflate the layout for this fragment
-//        return inflater.inflate(R.layout.settings_fragment, container, false);
+//        return inflater.inflate(R.layout.plans_page, container, false);
+//    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = super.onCreateView(inflater, container, savedInstanceState);
+        view.setBackgroundColor(getResources().getColor(R.color.teal_200));
+
+        return view;
+    }
+
+//    @Override
+//    public void onViewCreated(View view, Bundle savedInstanceState) {
+//        super.onViewCreated(view, savedInstanceState);
+//        view.setBackgroundColor(getResources().getColor(R.color.purple_200));
 //    }
 
     @Override
