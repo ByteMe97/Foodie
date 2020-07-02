c8cd12d930551c38ee0e93457ec3b76e24f4e7e3
Kendra <haanken@mail.gvsu.edu>
2020-07-02 15:27:07 -0400

    Moved Welcome label to activity_main and updated so it displays beneath toolbar

--- app/src/main/java/com/example/myfirstapp/WelcomePage.java
+++ app/src/main/java/com/example/myfirstapp/WelcomePage.java
@@ -18,8 +18,6 @@ public class WelcomePage extends Fragment {
             LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState
     ) {
-
-
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.welcome_page, container, false);
     }
