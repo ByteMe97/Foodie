7f64a1f0c7958c331492ddc23264e7fb8ed2d48f
ByteMe97 <labellk@mail.gvsu.edu>
2020-07-03 11:17:16 -0400

    set up

--- app/src/main/java/com/example/myfirstapp/SettingsPage.java
+++ app/src/main/java/com/example/myfirstapp/SettingsPage.java
@@ -1,38 +1,46 @@
 package com.example.myfirstapp;
 
 import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import androidx.preference.PreferenceFragmentCompat;
-
-public class SettingsPage extends PreferenceFragmentCompat {
-//    @Override
-//    public View onCreateView(
-//            LayoutInflater inflater, ViewGroup container,
-//            Bundle savedInstanceState
-//    ) {
-//        // Inflate the layout for this fragment
-//        return inflater.inflate(R.layout.plans_page, container, false);
-//    }
+public class SettingsPage extends Fragment {
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = super.onCreateView(inflater, container, savedInstanceState);
-        view.setBackgroundColor(getResources().getColor(R.color.teal_200));
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.settings, container, false);
+    }
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
 
-        return view;
+
+
+        view.findViewById(R.id.personal_info).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(SettingsPage.this)
+                        .navigate(R.id.action_SettingsPage_to_PersonalSettingsPage);
+            }
+        });
+
+        view.findViewById(R.id.password).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(SettingsPage.this)
+                        .navigate(R.id.action_SettingsPage_to_PasswordSettingsPage);
+            }
+        });
+
     }
 
-//    @Override
-//    public void onViewCreated(View view, Bundle savedInstanceState) {
-//        super.onViewCreated(view, savedInstanceState);
-//        view.setBackgroundColor(getResources().getColor(R.color.purple_200));
-//    }
-
-    @Override
-    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
-        setPreferencesFromResource(R.xml.root_preferences, rootKey);
-    }
 }
