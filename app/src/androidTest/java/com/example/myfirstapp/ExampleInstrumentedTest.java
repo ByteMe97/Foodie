76e2d377d7884c6c5f51f666674f90f0cad3eeaf
Kendra <haanken@mail.gvsu.edu>
2020-07-12 17:24:38 -0400

    Started on instrumentation testing

--- app/src/androidTest/java/com/example/myfirstapp/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/myfirstapp/ExampleInstrumentedTest.java
@@ -2,12 +2,23 @@ package com.example.myfirstapp;
 
 import android.content.Context;
 
+import androidx.test.filters.LargeTest;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import androidx.test.rule.ActivityTestRule;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.junit.Assert.*;
 
 /**
@@ -17,10 +28,27 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+//    @Test
+//    public void useAppContext() {
+//        // Context of the app under test.
+//        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+//        assertEquals("com.example.myfirstapp", appContext.getPackageName());
+//
+//    }
+
+    @Rule
+    //private String stringToBetyped = "Espresso";
+    public ActivityTestRule<MainActivity> activityRule
+            = new ActivityTestRule<>(MainActivity.class);
+
     @Test
-    public void useAppContext() {
-        // Context of the app under test.
+    public void changeText_sameActivity() {
+        // Type text and then press the button.
+        onView(withId(R.id.button_settings)).perform(click());
+
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.myfirstapp", appContext.getPackageName());
+        assertEquals("com.example.myfirstapp.settingspage", appContext.getApplicationContext());    // NEEDS WORK HERE!!
+
     }
+
 }
